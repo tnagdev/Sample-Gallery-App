c35025b2cd1f45a62507119d8b2d8d1a8c56f4e4
Tridibesh Nag <tnag@swiftsecurity.ai>
2024-10-07 11:28:14 +0530

    fix

--- src/atoms/searchAtom.tsx
+++ src/atoms/searchAtom.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { atom } from "jotai";
 
+type PexelState = {page: number, count: number, results: any[], total: number, query: string};
 
-export const photoAtom = atom({page: 1, count: 50, results: [], total: 0, query: ''} as {page: number, count: number, results: any[], total: number, query: string});
-export const videoAtom = atom({page: 1, count: 50, results: [], total: 0, query: ''} as {page: number, count: number, results: any[], total: number, query: string});
+export const photoAtom = atom({page: 1, count: 50, results: [], total: 0, query: ''} as PexelState);
+export const videoAtom = atom({page: 1, count: 50, results: [], total: 0, query: ''} as PexelState);
